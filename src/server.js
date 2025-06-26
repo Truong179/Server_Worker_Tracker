1f91bfe684658550d3c7b1d1167fa85709dd09c4
Truong179 <nguyenngoctruong1792003@gmail.com>
2025-06-26 16:05:56 +0700

    fix lan cuoi

--- src/server.js
+++ src/server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
 require("dotenv").config(); // thêm dòng này trên cùng
-const authRoutes = require("./src/routes/auth");
+const authRoutes = require("./routes/auth");
 
 const app = express();
 app.use(cors());
